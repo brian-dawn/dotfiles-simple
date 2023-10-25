a2e7dc112da9873e059228b1fdd5781b3421c451
Brian Dawn <brian.t.dawn@gmail.com>
2023-10-24 20:40:22 -0500

    Fix tab.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,6 +41,26 @@ require('lazy').setup({
     {'windwp/nvim-autopairs'}, --
     {'windwp/nvim-ts-autotag'}, --
 
+
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "debugloop/telescope-undo.nvim",
+      },
+      config = function()
+        require("telescope").setup({
+          extensions = {
+            undo = {
+              -- telescope-undo.nvim config, see below
+            },
+          },
+        })
+        require("telescope").load_extension("undo")
+        vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+      end,
+    }, --
+
     {
   "NeogitOrg/neogit",
   dependencies = {
