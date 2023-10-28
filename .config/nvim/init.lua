56f68fa0bd2ab915d75ef20540d4ebfe09373202
Brian Dawn <brian.t.dawn@gmail.com>
2023-10-28 08:02:49 -0500

    Remove the git blame hint since it's annoying.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,20 +86,8 @@ require("lazy").setup({
 		end,
 	}, --
 
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"nvim-telescope/telescope.nvim", -- optional
-			"sindrets/diffview.nvim", -- optional
-			"ibhagwan/fzf-lua", -- optional
-		},
-		config = true,
-	}, -- Neogit
 
 	{ "tpope/vim-fugitive" }, --
-
-	{ "f-person/git-blame.nvim" }, --
 
 	{ "phaazon/hop.nvim" }, --
 
