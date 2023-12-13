186ba6c05253add3708cc1767b5451f9a9a91ce4
Brian Dawn <brian.t.dawn@gmail.com>
2023-12-12 18:59:57 -0600

    Change scroll animation length.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -328,7 +328,7 @@ if vim.g.neovide then
 	vim.g.neovide_cursor_vfx_mode = ""
 	vim.g.neovide_input_macos_alt_is_meta = true
 	vim.g.neovide_cursor_trail_size = 0.0
-	vim.g.neovide_scroll_animation_length = 0.005
+	vim.g.neovide_scroll_animation_length = 0
 
 	vim.g.neovide_cursor_animate_in_insert_mode = true
 	vim.g.neovide_scale_factor = 1.0
