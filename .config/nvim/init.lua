684569ac80b02fadbbca2faeb20dcb44e1f4edee
Brian Dawn <brian.t.dawn@gmail.com>
2024-06-11 09:47:43 -0500

    Update.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -326,7 +326,7 @@ if vim.g.neovide then
 	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 
 	vim.g.neovide_cursor_vfx_mode = ""
-    vim.g.neovide_input_macos_option_key_is_meta = true
+    vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 	vim.g.neovide_cursor_trail_size = 0.0
 	vim.g.neovide_scroll_animation_length = 0
 
