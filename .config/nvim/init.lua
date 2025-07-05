2991042467d047c0ffe136d68882ee20ee4ca372
Brian Dawn <brian.t.dawn@gmail.com>
2025-07-05 11:52:08 -0700

    Autosave.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,15 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "FocusGained" }, {
   command = "checktime"
 })
 
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.cmd("silent! write")
+    end
+  end,
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
