59b3e721d224fa37f249a655796e4c1210df47c6
Brian Dawn <brian.t.dawn@gmail.com>
2025-07-09 21:19:13 -0700

    Auto pairs.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -158,6 +158,16 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>.", builtin.resume, { desc = "Resume last search" })
     end,
   },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({})
+      
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
 })
 
 
